34d633e8e80697ffa4246aeac597931ddd34cf84
wanwan510 <kanwunwan@gmail.com>
2024-09-20 16:08:13 +0800

    20092024

--- bmi.js
+++ bmi.js
@@ -3,3 +3,15 @@ var height=160;
 var bmi=weight/((height/100)**2);
 
 console.log("Hello!Your BMI value is"+ bmi)
+
+function calcBMI(){
+    //var weight=50;
+    var height=
+    Number(document.getElementsById("height").value);
+    var weight=
+    document.getElementById("weight").value;
+    var bmi=weight/((height/100)**2);
+    document.getElementById("bmi").innerHTML=bmi;
+    console.log("Hello, Your BMI is:" + bmi);
+}
+IPO
